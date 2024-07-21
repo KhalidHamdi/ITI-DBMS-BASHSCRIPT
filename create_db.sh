affb84475dd425e17d463f4cc4f6866011064917
KhalidHamdi <khalidhamdi204@gmail.com>
2024-07-22 00:19:27 +0300

    fix: enhance validation checks in create_db.sh
    Improves validation logic in create_db.sh by:
    - Ensuring the database name is not empty.
    - Checking that the database name starts with a letter.
    - Validating that the database name contains only allowed characters (letters, numbers, and underscores).

--- create_db.sh
+++ create_db.sh
@@ -5,10 +5,24 @@ create_database() {
     while true; 
     do
         read -p "Please enter database name (or type 'exit' to quit): " db_name
-
         if [[ "$db_name" == "exit" ]]; then
             echo "Exiting the database creation process."
             break
+        fi
+
+        if [[ -z "$db_name" ]]; then
+            echo "Database name cannot be empty. Please try again with a valid name."
+            continue
+        fi
+
+        if [[ ! "$db_name" =~ ^[a-zA-Z] ]]; then
+            echo "Database name must start with a letter. Please try again with a valid name."
+            continue
+        fi
+
+        if [[ ! "$db_name" =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]; then
+            echo "Database name contains invalid characters. Only letters, numbers, and underscores are allowed. Please try again with a valid name."
+            continue
         fi
 
         if [ -d "$db_path/$db_name" ]; then
