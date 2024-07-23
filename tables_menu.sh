d7809667f624ac7238c9a89f9ef1c26c3a95c445
KhalidHamdi <khalidhamdi204@gmail.com>
2024-07-23 19:21:51 +0300

    feat: Update tables_menu script to include select and update table options
    - Added options to select from and update a table in the tables menu.

--- tables_menu.sh
+++ tables_menu.sh
@@ -8,9 +8,11 @@ tables_menu() {
         echo "2. List Tables"
         echo "3. Drop Table"
         echo "4. Insert Into Table"
-        echo "5. Back to Main Menu"
+        echo "5. Select From Table"
         echo "6. Delete From Table"
-        
+        echo "7. Update Table"
+        echo "8. Back to Main Menu"
+
         read -p "Please select an option: " db_menu_option
 
         case $db_menu_option in
@@ -18,9 +20,11 @@ tables_menu() {
             2)clear; . ./list_tables.sh ;;  
             3)clear; . ./drop_table.sh ;;
             4)clear; . ./insert_into_table.sh ;;
-            5)clear; . ./mainMenu.sh ;;
+            5)clear; . ./select_from_table.sh ;;
             6)clear; . ./delete_from_table.sh ;;
-            *) echo "Invalid option. Please select a valid option." ;;
+            7)clear; . ./update_table.sh ;;
+            8)clear; . ./mainMenu.sh ;;
+            *)clear; echo "Invalid option. Please select a valid option." ;;
         esac
     done
 }
