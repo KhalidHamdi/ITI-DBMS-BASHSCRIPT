00417ecfd624e41a6ca0bc7b2b358bc2f26f1911
ahmad <ahmadabdulmuniem8@gmail.com>
2024-07-23 21:12:57 +0300

    refactor: editing the features of the delete options and adding contraints.

--- delete_from_table.sh
+++ delete_from_table.sh
@@ -1,40 +1,145 @@
 #!/bin/bash
-delete_from_table(){
-    table_loc="./databases"
-    echo "Enter the table name please: "
-    read table_name
-    folder_path="$table_loc/$table_name/data.txt"
-    
-    if [ -f "$folder_path" ]; then
-        echo "Do you want to delete a row (y/n): "
-        read answer
-        if [ "$answer" = "y" ]; then
-            echo "Enter the primary key for the row you want to delete: "
-            read pk
-            awk -F ' ' -v pk="$pk" '$2 != pk {print $0}' "$folder_path" > temp.txt
-            mv temp.txt "$folder_path"
-            echo "Record with primary key '$pk' has been deleted."
-        else
-            echo "Do you want to delete a full column (y/n): "
-            read choice
-            if [ "$choice" = "n" ]; then
-                echo "Enter the column name: "
-                read column_name
-                case "$column_name" in
-                    name) column_number=1;;
-                    age) column_number=2;;
-                    edu) column_number=3;;
-                    *) echo "Invalid column name, please enter a valid name"; exit 1 ;;
-                esac
-                awk -v col="$column_number" '{ $col=""; $1=$1; print }' "$folder_path" > temp_file
-                mv temp_file "$folder_path"
-                echo "Column '$column_name' has been deleted."
-            else
-                echo "Operation cancelled."
-            fi
+#!/bin/bash
+delete_from_table() {
+    while true; do
+        echo -n 'Enter table name (or type "exit" to cancel): '
+        read table_name
 
+        if [[ "$table_name" == "exit" ]]; then
+            clear
+            echo "Operation canceled."
+            read -n 1 -s -r -p "Press any key to go to the tables menu..."
+            clear
+            . ./tables_menu.sh
+            exit 0
+        elif [[ -z "$table_name" ]]; then
+            clear
+            echo "You did not enter a table name. Please try again or type 'exit' to cancel."
+            continue
         fi
-    fi
-}
 
+        table_path="$CURRENT_DB/$table_name"
+        data_path="$table_path/data.txt"
+        metadata_path="$table_path/metadata.txt"
+
+        if [[ -d "$table_path" ]]; then
+            break
+        else
+            echo "Table '$table_name' does not exist. Please try again or type 'exit' to cancel."
+        fi
+    done
+
+    while true; do
+        echo -n "Do you want to delete by primary key (y/n): "
+        read -n 1 -s -r answer
+        clear
+
+        if [ "$answer" = "y" ]; then
+            while true; do
+                echo -n "Enter the primary key for the row you want to delete (or type 'exit' to cancel): "
+                read pk
+
+                if [[ "$pk" == "exit" ]]; then
+                    clear
+                    echo "Operation canceled."
+                    read -n 1 -s -r -p "Press any key to go to the tables menu..."
+                    clear
+                    . ./tables_menu.sh
+                    exit 0
+                fi
+
+                if grep -q "^$pk:" "$data_path"; then
+                    echo "Primary key '$pk' exists in data file."
+
+                    pk_column_num=$(awk -F ':' '$3 == "y" {print NR; exit}' "$metadata_path")
+
+                    if [ -z "$pk_column_num" ]; then
+                        echo "Primary key column not found in metadata. Operation cannot proceed."
+                        read -n 1 -s -r -p "Press any key to go back..."
+                        clear
+                        continue
+                    fi
+
+                    awk -v pk="$pk" -v pk_col="$pk_column_num" -F ':' '$pk_col != pk' "$data_path" > temp.txt
+                    mv temp.txt "$data_path"
+                    clear
+                    echo "Record with primary key '$pk' has been deleted."
+                    read -n 1 -s -r -p "Press any key to go back..."
+                    clear
+                    break
+                else
+                    echo "Primary key '$pk' does not exist. Please try again."
+                fi
+            done
+            break
+        elif [ "$answer" = "n" ]; then
+            while true; do
+                echo -n "Do you want to delete a full column (y/n): "
+                read -n 1 -s -r choice
+                clear
+
+                if [ "$choice" = "y" ]; then
+                    while true; do
+                        echo -n "Enter the column name (or type 'exit' to cancel): "
+                        read column_name
+
+                        if [[ "$column_name" == "exit" ]]; then
+                            clear
+                            echo "Operation canceled."
+                            read -n 1 -s -r -p "Press any key to go to the tables menu..."
+                            clear
+                            . ./tables_menu.sh
+                            exit 0
+                        fi
+
+                        column_number=$(awk -F ':' -v col_name="$column_name" '$1 == col_name {print NR}' "$metadata_path")
+
+                        if [ -n "$column_number" ]; then
+                            break
+                        else
+                            echo "Invalid column name. Please try again or type 'exit' to cancel."
+                        fi
+                    done
+
+                    pk_column_number=$(awk -F ':' '$3 == "y" {print NR}' "$metadata_path")
+
+                    if [ "$column_number" -eq "$pk_column_number" ]; then
+                        echo "Warning: The column you are about to delete is the primary key column. This will remove the primary key constraint and may affect data integrity. Are you sure you want to proceed? (y/n)"
+                        read -n 1 -s -r confirm
+                        clear
+
+                        if [ "$confirm" != "y" ]; then
+                            clear
+                            echo "Operation canceled."
+                            read -n 1 -s -r -p "Press any key to go back..."
+                            clear
+                            continue
+                        fi
+                    fi
+
+                    awk -F ':' -v col="$column_number" '{ for (i = 1; i <= NF; i++) if (i != col) printf "%s%s", $i, (i < NF && i != col - 1) ? ":" : "\n" }' "$data_path" > temp_file
+                    mv temp_file "$data_path"
+                    clear
+                    echo "Column '$column_name' has been deleted."
+                    read -n 1 -s -r -p "Press any key to go back..."
+                    clear
+                    break
+
+                elif [ "$choice" = "n" ]; then
+                    clear
+                    echo "Operation canceled."
+                    read -n 1 -s -r -p "Press any key to go to the tables menu..."
+                    clear
+                    . ./tables_menu.sh
+                    exit 0
+                else
+                    echo "Invalid input. Please enter 'y' or 'n'."
+                fi
+            done
+            break
+        else
+            echo "Invalid input. Please enter 'y' or 'n'."
+        fi
+    done
+}
 delete_from_table
